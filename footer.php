771beee0d9d6479375746d57c5f62225ea7cf2f1
B Hicks <b@audiblePi.com>
2015-12-07 14:29:23 -0500

    updated dynamic footer, custom profile form page

--- footer.php
+++ footer.php
@@ -14,7 +14,29 @@
         <div class="grid col-300 scroll-top"><a href="#scroll-top" title="<?php esc_attr_e( 'scroll to top', 'shell' ); ?>"><?php _e( '&uarr;', 'shell' ); ?></a></div>
         
         <div class="grid col-300 fit powered">
-            <p><?php wp_nav_menu( array( 'theme_location' => 'admin') ); ?></p>
+            <ul class="admin-nav">
+                <?php
+                    $current_user = wp_get_current_user();
+                    $current_user_name = $current_user->display_name;
+                    if ( is_user_logged_in() ) {
+                        ?>
+                        <script>
+                            jQuery(function($){
+                                $('.welcome-msg').css('display', 'inline-block');
+                                $('.menu-admin-container ul li:nth-child(1)').css('display', 'inline-block');
+                                $('.menu-admin-container ul li:nth-child(2)').css('border-left', '1px dotted #919191');
+                            });
+                        </script>
+                        <?
+                    }
+                ?>
+                <li class="welcome-msg">
+                    Welcome, <?php echo $current_user_name ?>!
+                </li>
+                <li>
+                    <?php wp_nav_menu( array( 'theme_location' => 'admin') ); ?>
+                </li>
+            </ul>
         </div><!-- end .powered -->
     </div><!-- end #footer-wrapper -->
 </div><!-- end #footer -->
--- templates/dashboard.php
+++ templates/dashboard.php
@@ -32,10 +32,8 @@ date_default_timezone_set('EST');
             <ul class="nav nav-tabs nav-stacked">
             	<li class="nav-header">Main Navigation</li>
                 <li id="dashboard" class="active" ><a href="dashboard.html"><span class="icon-align-justify"></span> Dashboard</a></li>
-                <!-- <li id="forum"><a href=""><span class="icon-comment"></span> Forum</a></li> -->
                 <li id="calendar"><a href=""><span class="icon-calendar"></span> Calendar</a></li>
                 <li id="edit-profile"><a href=""><span class="icon-edit"></span>Edit Profile</a></li>
-                <!-- <li id="members"><a href=""><span class="icon-user"></span>Members</a></li> -->
             </ul>
         </div>
     </div>
@@ -45,35 +43,13 @@ date_default_timezone_set('EST');
         	<a href="" class="showmenu"></a>
             
             <div class="headerright">
-            	<div class="dropdown notification">
-                    <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="/page.html">
-                    	<span class="iconsweets-globe iconsweets-white"></span>
-                    </a>
-                    <ul class="dropdown-menu">
-                    	<li class="nav-header">Notifications</li>
-                        <li>
-                        	<a href="">
-                        	<strong>3 people viewed your profile</strong><br />
-                            <img src="/wp-content/plugins/fcc-dashboard/img/thumbs/thumb1.png" alt="" />
-                            <img src="/wp-content/plugins/fcc-dashboard/img/thumbs/thumb2.png" alt="" />
-                            <img src="/wp-content/plugins/fcc-dashboard/img/thumbs/thumb3.png" alt="" />
-                            </a>
-                        </li>
-                        <li><a href=""><span class="icon-envelope"></span> New message from <strong>Jack</strong> <small class="muted"> - 19 hours ago</small></a></li>
-                        <li><a href=""><span class="icon-envelope"></span> New message from <strong>Daniel</strong> <small class="muted"> - 2 days ago</small></a></li>
-                        <li><a href=""><span class="icon-user"></span> <strong>Bruce</strong> is now following you <small class="muted"> - 2 days ago</small></a></li>
-                        <li class="viewmore"><a href="">View More Notifications</a></li>
-                    </ul>
-                </div>
-                
     			<div class="dropdown userinfo">
-                    <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="/page.html">Hi, <?php echo $current_user->first_name ?> <b class="caret"></b></a>
+                    <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="/page.html">Hi, <?php echo $current_user->display_name ?> <b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li id="edit-profile"><a href=""><span class="icon-edit"></span> Edit Profile</a></li>
-                        <li id="account-settings"><a href=""><span class="icon-wrench"></span> Account Settings</a></li>
-                        <li id="privacy-settings"><a href=""><span class="icon-eye-open"></span> Privacy Settings</a></li>
+                        <li id="edit-profile" class="script"><a href=""><span class="icon-edit"></span> Edit Profile</a></li>
+                        <li id="back-home"><a href="<?php echo site_url(); ?>"><span class="iconsweets-globe"></span> Back to Home</a></li>
                         <li class="divider"></li>
-                        <li id="logout"><a href="/logout"><span class="icon-off"></span> Sign Out</a></li>
+                        <li id="logout"><a href="<?php echo wp_logout_url(); ?>"><span class="icon-off"></span> Sign Out</a></li>
                     </ul>
                 </div>
             </div>
@@ -95,29 +71,11 @@ date_default_timezone_set('EST');
 		        <div class="dynamic-content-wrapper dashboard current">
 			        <?php echo do_shortcode('[fcc-dashboard]'); ?>
 		    	</div>
-
-		    	<div class="dynamic-content-wrapper forum">
-			        <?php echo do_shortcode('[fcc-forum]'); ?>
-		    	</div>
-
 		    	<div class="dynamic-content-wrapper calendar">
 			        <?php echo do_shortcode('[fcc-calendar]'); ?>
 		    	</div>
-
 		    	<div class="dynamic-content-wrapper edit-profile">
 			        <?php echo do_shortcode('[fcc-edit-profile]'); ?>
-		    	</div>
-
-		    	<div class="dynamic-content-wrapper members">
-			        <?php echo do_shortcode('[fcc-members]'); ?>
-		    	</div>
-
-		    	<div class="dynamic-content-wrapper account-settings">
-			        <?php echo do_shortcode('[fcc-account-settings]'); ?>
-		    	</div>
-
-		    	<div class="dynamic-content-wrapper privacy-settings">
-			        <?php echo do_shortcode('[fcc-privacy-settings]'); ?>
 		    	</div>
 		    </div>
 		  </div>
@@ -127,7 +85,7 @@ date_default_timezone_set('EST');
     <div class="clearfix"></div>
     
     <div class="footer">
-    	<div class="footerleft">Katniss Premium Admin Template v1.0</div>
-    	<div class="footerright">&copy; ThemePixels - <a href="http://twitter.com/themepixels">Follow me on Twitter</a> - <a href="http://dribbble.com/themepixels">Follow me on Dribbble</a></div>
+    	<div class="footerleft"><a href="<?php echo site_url(); ?>">&copy; <?php echo date('Y'); ?> Florida Career Centers</a></div>
+    	<div class="footerright"><a href="http://www.pippindesign.com">Website Design & Development</a> by Pippin Design</div>
     </div>
 </div>
